49b144fdedb23c4ebc3328f5f65f35064c472779
Ignacio Castro <ignaciocastrom@gmail.com>
2024-10-24 22:02:21 -0300

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -49,8 +49,8 @@ module.exports = {
   // you provide. See the documentation for details.
   links: [
     {
-      name: 'GitHub',
-      link: 'https://github.com/lyc8503',
+      name: 'Discord',
+      link: 'https://discord.hgsn.space',
     },
   ],
 
