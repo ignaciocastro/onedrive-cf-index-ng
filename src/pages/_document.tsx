94b5396959386344b90559a542d22c4a94ad8335
Ignacio Castro <ignaciocastrom@gmail.com>
2024-10-24 23:05:04 -0300

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,19 +7,26 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="description" content="A fan-run, semi public archive to all things HGSN (and its author)" />
-          <meta property="og:description" content="A fan-run, semi public archive to all things HGSN (and its author)" />
-          <meta property="og:title" content={siteConfig.title} />
           <meta name="robots" content="noindex" />
+          
+          {/* OpenGraph Tags */}
+          <meta property="og:type" content="website" />
+          <meta property="og:site_name" content={siteConfig.title} />
+          <meta property="og:url" content="https://hgsn.space" />
+          <meta property="og:title" content={siteConfig.title} />
+          <meta property="og:description" content="A fan-run, semi public archive to all things HGSN (and its author)" />
+          <meta property="og:image" content="https://hgsn.space/banner.jpg" />
+          <meta property="og:image:width" content="1280" />
+          <meta property="og:image:height" content="640" />
+          <meta property="og:image:type" content="image/jpeg" />
+          
+          {/* Favicon and Fonts */}
           <link rel="icon" href="/favicon.ico" />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
           {siteConfig.googleFontLinks.map(link => (
             <link key={link} rel="stylesheet" href={link} />
           ))}
-          <meta property="og:image" content="https://hgsn.space/banner.jpg" />
-          <meta name="twitter:image" content="https://hgsn.space/banner.jpg" />
-          <meta property="og:image:width" content="1280" />
-          <meta property="og:image:height" content="640" />
         </Head>
         <body>
           <Main />
